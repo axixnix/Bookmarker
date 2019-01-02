27d41b7db8df36d99aa7241ae2866010dfb81da5
axixnix <henryirons@yahoo.com>
2019-01-02 23:44:36 +0100

    more local storage adding visit and delete functionality

--- js/main.js
+++ js/main.js
@@ -18,3 +18,28 @@ function saveBookmark (e) {
 
   e.preventDefault()
 }
+
+function deleteBookmark (url) {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+}
+
+function fetchBookmarks () {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+
+  var bookmarksResults = document.getElementById('bookmarksResults')
+
+  console.log(bookmarksResults)
+  bookmarksResults.innerHTML = ''
+  for (var i = 0;i < bookmarks.length;i++) {
+    var name = bookmarks[i].name
+    var url = bookmarks[i].url
+
+    console.log(name)
+    bookmarksResults.innerHTML += '<div class="well">' +
+      '<h3>' + name +
+      '<a class="btn btn-default" target ="_blank" href="' + url + '">Visit</a>' +
+      '<a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger"  href="#">Delete</a>' +
+      '</h3>' +
+      '</div>'
+  }
+}
